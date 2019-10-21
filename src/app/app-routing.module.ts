5cf7417cf213aec44218d97794921c1e9b919260
Martin Cserép <martincserep1@gmail.com>
2019-10-21 11:00:14 +0200

    auth page getting started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: 'main', loadChildren: './main/main.module#MainPageModule' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
 ];
